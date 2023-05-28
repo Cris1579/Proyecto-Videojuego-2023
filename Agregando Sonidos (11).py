8df5f8bfde70f35f8562f63cd9670aa542e2ba50
Cris <cristopher.gonzalezguardaccp@gmail.com>
2023-05-28 13:54:01 -0400

    :P

--- Agregando Sonidos (11).py
+++ Agregando Sonidos (11).py
@@ -3,6 +3,7 @@
 #Se inventa una variable con el sonido
 
 # Sonido = pygame.mixer.Sound ("Nombre del sonido.ogg")
-# Dentro del bucle, se le asigna una tecla
+# Dentro del bucle, se le asigna a un evento (ya sea una tecla o una acción dentro del juego)
 # y se coloca la siguiente línea
 # sound.play()
+
